19918074890c4b440a9dca59664e4ad3e7284302
Your Name <you@example.com>
2023-06-21 13:04:28 +0300

    conez!

--- server.js
+++ server.js
@@ -15,33 +15,33 @@ async function getDbCollectoin(dbAddress, dbName, dbCollectionName){
 	return db.collection(dbCollectionName);
 }
 
-app.get('/tasks', async function(req, res){
-	const collection = await getDbCollectoin('mongodb://127.0.0.1','todoapp', 'tasks');
+app.get('/zakaz', async function(req, res){
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	const data = await collection.find({}).toArray();
 	res.send(data);
 });
 
-app.get('/tasks/:id', async function(req, res){
-	const collection = await getDbCollectoin('mongodb://127.0.0.1','todoapp', 'tasks');
+app.get('/zakaz/:id', async function(req, res){
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	const data = await collection.findOne({_id: new ObjectId(req.params.id)});
 	res.send(data);
 });
 
-app.post('/tasks', async function(req, res){
+app.post('/zakaz', async function(req, res){
 	const task={...req.body, done: false}
-	const collection = await getDbCollectoin('mongodb://127.0.0.1','todoapp', 'tasks');
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	await collection.insertOne(task);
 	res.send(task);
 });
 
-app.patch('/tasks/:id', async function(req, res){
-	const collection = await getDbCollectoin('mongodb://127.0.0.1','todoapp', 'tasks');
+app.patch('/zakaz/:id', async function(req, res){
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	const data = await collection.updateOne({_id: new ObjectId(req.params.id)}, {'$set': req.body});
 	res.send({});
 });
 
-app.delete('/tasks/:id', async function(req, res){
-	const collection = await getDbCollectoin('mongodb://127.0.0.1','todoapp', 'tasks');
+app.delete('/zakaz/:id', async function(req, res){
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	await collection.deleteOne({_id: new ObjectId(req.params.id)});
 	res.send({});
 });
